2ec5bfed376f56dfa6da051e9b67897d60c364fc
FernandoMoyano <fernandomoyano21@gmail.com>
2023-08-16 19:52:27 -0300

    residencyRoute

---
+++ server/controllers/residencyController.js
@@ -0,0 +1,39 @@
+import asyncHandler from "express-async-handler";
+import { prisma } from "../config/prismaConfig.js";
+
+export const createResidency = asyncHandler(async (req, res) => {
+  const {
+    title,
+    description,
+    price,
+    adress,
+    country,
+    city,
+    facilities,
+    image,
+    userEmail,
+  } = req.body.data;
+
+  console.log(req.body.data);
+  try {
+    const residency = await prisma.residency.create({
+      data: {
+        title,
+        description,
+        price,
+        adress,
+        country,
+        city,
+        facilities,
+        image,
+        owner: { connect: { email: userEmail } },
+      },
+    });
+    res.send({ message: "Residency creted successfully", residency });
+  } catch (err) {
+    if (err.code === "P2002") {
+      throw new Error("A residency with address already there");
+    }
+    throw new Error(err.message);
+  }
+});
