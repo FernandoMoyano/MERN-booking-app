fd666184c81a93c9d24b4f4507bb12c998fa863b
FernandoMoyano <fernandomoyano21@gmail.com>
2023-08-17 14:19:01 -0300

    getAllResidencies

--- server/routes/residencyRoute.js
+++ server/routes/residencyRoute.js
@@ -1,7 +1,11 @@
 import express from "express";
-import { createResidency } from "../controllers/residencyController.js";
+import {
+  createResidency,
+  getAllResidencies,
+} from "../controllers/residencyController.js";
 const router = express.Router();
 
 router.post("/create", createResidency);
+router.get("/allresidencies", getAllResidencies);
 
 export { router as residencyRoute };
