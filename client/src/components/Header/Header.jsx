35c4bc752d1263eab1da45d2dadc24d5a154790b
FernandoMoyano <fernandomoyano21@gmail.com>
2023-08-25 18:11:48 -0300

    Hero

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./Header.css"
 
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <section className="h-wrapper">
+      <div className="flexCenter paddings innerWidth h-menu h-container">
+        <img src="../public/logo.png" alt="logo" width={100} />
+        <div className=" flexCenter h-menu">
+          <a href="">Residencies</a>
+          <a href="">Our Value</a>
+          <a href="">Contact us</a>
+          <a href="">Get started</a>
+          <button className="button">
+            <a href="">Contact</a>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Header
--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import "./Hero.css"
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="hero-wrapper">
+      <div className="paddings innerWidth flexCenter hero-container">
+        <div className="hero-left">Left section</div>
+        <div className="flexCenter hero-right">
+          <div className="image-container">
+            <img src="../../../public/hero-image.png" alt="hero-img"></img>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
