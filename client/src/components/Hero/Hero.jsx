934f1a67047cf6a409932ccb45e4eddddddc8287
FernandoMoyano <fernandomoyano21@gmail.com>
2023-08-26 13:39:40 -0300

    Hero-styles

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,11 +1,31 @@
 import React from "react";
-import "./Hero.css"
+import "./Hero.css";
+import "../../index.css"
 
 const Hero = () => {
   return (
     <section className="hero-wrapper">
       <div className="paddings innerWidth flexCenter hero-container">
-        <div className="hero-left">Left section</div>
+        <div className="flexColStart hero-left">
+          <div className="hero-title">
+            <div className="orange-circle" />
+            <h1>
+              Discover<br />
+              Most Suitable
+              <br />Property
+            </h1>
+          </div>
+
+          <div className="flexColStart secondaryText hero-des">
+            <span>Find a variety of properties that suit you very easilty</span>
+            <span>Forget all difficulties in finding a residence for you</span>
+          </div>
+
+          <div className="search-bar">
+            Search bar
+          </div>
+        </div>
+
         <div className="flexCenter hero-right">
           <div className="image-container">
             <img src="../../../public/hero-image.png" alt="hero-img"></img>
