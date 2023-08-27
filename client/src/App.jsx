b47e0f8d12959bbb16476da96031a1a394dc3c9c
FernandoMoyano <fernandomoyano21@gmail.com>
2023-08-27 14:54:36 -0300

    Residencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import "./app.css";
 import Companies from "./components/companies/Companies";
+import Residencies from "./components/Residencies/Residencies";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Hero />
       </div>
       <Companies />
+      <Residencies/>
     </div>
   );
 }
