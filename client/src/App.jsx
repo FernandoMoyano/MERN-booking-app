26e404c70da12ef4dcf7109dec402656934bad8f
FernandoMoyano <fernandomoyano21@gmail.com>
2023-08-27 13:42:23 -0300

    Companies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import "./app.css";
+import Companies from "./components/companies/Companies";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <Hero />
       </div>
+      <Companies />
     </div>
   );
 }
---
+++ client/src/components/companies/Companies.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Companies.css";
+
+const Companies = () => {
+  return (
+    <section className="c-wrapper">
+      <div className="paddings innerWidth flexCenter c-container">
+        <img src="../../../public/prologis.png" alt=""></img>
+        <img src="../../../public/tower.png" alt=""></img>
+        <img src="../../../public/equinix.png" alt=""></img>
+        <img src="../../../public/realty.png" alt=""></img>
+      </div>
+    </section>
+  );
+};
+
+export default Companies;
